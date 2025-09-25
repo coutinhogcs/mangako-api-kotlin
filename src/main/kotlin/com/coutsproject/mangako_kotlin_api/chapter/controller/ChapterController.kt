1bde14d5c149a6d6057278607ca8641b4e547163
Guilherme Coutinho <coutinhocguilherme@gmail.com>
2025-09-24 23:20:26 -0300

    feat: Add swagger

--- src/main/kotlin/com/coutsproject/mangako_kotlin_api/chapter/controller/ChapterController.kt
+++ src/main/kotlin/com/coutsproject/mangako_kotlin_api/chapter/controller/ChapterController.kt
@@ -3,6 +3,7 @@ package com.coutsproject.mangako_kotlin_api.chapter.controller
 import com.coutsproject.mangako_kotlin_api.chapter.dto.ChapterRequestDTO
 import com.coutsproject.mangako_kotlin_api.chapter.dto.ChapterResponseDTO
 import com.coutsproject.mangako_kotlin_api.chapter.service.ChapterService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -11,14 +12,17 @@ import org.springframework.web.bind.annotation.*
 class ChapterController(private val service: ChapterService) {
 
     @GetMapping
+    @Operation(summary = "List all", description = "List all manga")
     fun findAll(): List<ChapterResponseDTO> = service.findAll()
 
     @PostMapping("/{id}")
+    @Operation(summary = "Create a new chapter", description = "Create a new chapter by id")
     fun addingNewChapter(@PathVariable id: Long, @RequestBody dto: ChapterRequestDTO ): ResponseEntity<ChapterResponseDTO>{
         return ResponseEntity.ok(service.addNewChapter(id, dto))
     }
 
     @PutMapping("/manga/{id}")
+    @Operation(summary = "Update a new chapter", description = "Update a new chapter by id")
     fun updateChapter(@PathVariable id:Long,@RequestBody dto: ChapterRequestDTO): ResponseEntity<ChapterResponseDTO>{
         return ResponseEntity.ok(service.changeChapter(id,dto))
     }
--- src/main/kotlin/com/coutsproject/mangako_kotlin_api/manga/controller/MangaController.kt
+++ src/main/kotlin/com/coutsproject/mangako_kotlin_api/manga/controller/MangaController.kt
@@ -4,34 +4,44 @@ import com.coutsproject.mangako_kotlin_api.manga.domain.Manga
 import com.coutsproject.mangako_kotlin_api.manga.dto.MangaRequestDTO
 import com.coutsproject.mangako_kotlin_api.manga.dto.MangaResponseDTO
 import com.coutsproject.mangako_kotlin_api.manga.service.MangaService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/manga")
+@Tag(name = "Manga", description = "Endpoints for manga management")
 class MangaController(final val service: MangaService) {
     @GetMapping
+    @Operation(summary = "List all", description = "Select all manga")
     fun getAllMangas(): ResponseEntity<List<Manga>>{
         return ResponseEntity.ok(service.getAll())
     }
 
     @GetMapping("/title/{title}")
+    @Operation(summary = "List by id", description = "Selects sleeves from their id")
     fun getTitle(@PathVariable title: String): ResponseEntity<MangaResponseDTO>{
         return ResponseEntity.ok(service.findByTitle(title))
     }
 
     @PostMapping
+    @Operation(summary = "Add a new manga", description = "Creates a new manga ")
     fun create(@RequestBody data: MangaRequestDTO): ResponseEntity<MangaResponseDTO>{
         return ResponseEntity.ok(service.insertManga(data))
     }
 
     @DeleteMapping("/title/{title}")
+    @Operation(summary = "Delete a new manga", description = "Delete a manga from your id")
     fun delete(@PathVariable title: String): ResponseEntity<MangaResponseDTO>{
        val deleted = service.deleteByTitle(title)
         return ResponseEntity.notFound().build()
     }
 
     @PutMapping("/title/{id}/update")
+    @Operation(summary = "Update a new manga", description = "Edit a manga from your id")
     fun updateManga( @RequestBody dto: MangaRequestDTO, @PathVariable id: Long): ResponseEntity<MangaResponseDTO>{
         return ResponseEntity.ok(service.changeManga(dto, id))
     }
